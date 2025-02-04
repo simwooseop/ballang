b6e7a5e6594bc7c7743f76d9a853cb45c07cfac7
심우섭 <sybiyong2@gmail.com>
2025-02-04 14:18:36 +0900

    debug: api

--- src/api/chat.ts
+++ src/api/chat.ts
@@ -2,7 +2,7 @@ import { Chat } from "@/types/chat";
 import axios from "axios";
 
 const getMessages = async (userId: string) => {
-  const { data } = await axios.get<Chat[]>("http://15.164.51.44:3000/chats", {
+  const { data } = await axios.get<Chat[]>("https://ballang.kro.kr/chats", {
     params: { roomId: userId },
   });
   const messages = data.map((msg) => `${msg.userName} : ${msg.message}`);
